adc2dfe988706db27823f777211dd4834f64f11a
maizer12 <101927316+maizer12@users.noreply.github.com>
2022-04-12 19:44:10 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+  $(function(){
+    $('.people-slider__items').slick({
+        arrows:false,
+        dots:true,
+        autoplay:true,
+        autoplaySpeed:4000,
+        speed:2000
+    });
+  });
