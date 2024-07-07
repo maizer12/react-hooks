0d7f19a93ad1ba0efb6f07ab0e10f01c9837b292
Maizer <vasilbazhiv12@gmail.com>
2024-07-07 16:40:45 +0300

    add two very important hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useHover } from './hooks/useHover';
+import { useState, useRef } from 'react';
+import { useClickOutside } from './hooks/useClickOutside';
 
 function App() {
-	const [hoverRef, isHovered] = useHover();
+	const [isOpen, setIsOpen] = useState(false);
+	const ref = useRef();
+
+	useClickOutside(ref, () => setIsOpen(false));
 
 	return (
 		<div className='main'>
 			<h1 className='title'>Custom Hooks</h1>
-			<div ref={hoverRef}>{isHovered ? 'Hover is true!' : 'Hover is false!'}</div>
+			<div>
+				<button onClick={() => setIsOpen(!isOpen)}>{isOpen ? 'Close' : 'Open'} Dropdown</button>
+				{isOpen && (
+					<div ref={ref} style={{ border: '1px solid black', padding: '10px', marginTop: '10px' }}>
+						<p>This is a Dropdown menu. Click outside to close it!</p>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }
