2d8c2162e42e75701873ced08d1123e0be452f25
Maizer <vasilbazhiv12@gmail.com>
2024-07-19 13:39:59 +0300

    useWindowScroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react';
-import { useClipboard } from './hooks/useClipboard';
+import { useWindowScroll } from './hooks/useWindowScroll';
 
 function App() {
-	const [text, setText] = useState('Hello, World!');
-	const { isCopied, copyToClipboard } = useClipboard();
+	const { x, y } = useWindowScroll();
 
 	return (
-		<div className='main'>
+		<div className='main' style={{ height: 2000, width: 7000 }}>
 			<h1 className='title'>Custom Hooks</h1>
-			<textarea value={text} onChange={e => setText(e.target.value)} />
-			<button onClick={() => copyToClipboard(text)}>Copy Text</button>
-			{isCopied && <p>Copied!</p>}
+			<p>X: {x}px</p>
+			<p>Y: {y}px</p>
 		</div>
 	);
 }
